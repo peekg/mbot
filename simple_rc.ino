8719d37fb32e9324773a2a16915b21cf3b332352
peekg <gaza.nz@gmail.com>
2017-10-27 19:15:43 -0400

    created simple rc bot, with ric remote

---
+++ simple_rc.ino
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <MeMegaPi.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+
+MeDCMotor motor_9(9);
+MeDCMotor motor_10(10);
+void move(int direction, int speed)
+{
+      int leftSpeed = 0;
+      int rightSpeed = 0;
+      if(direction == 1){
+          leftSpeed = speed;
+          rightSpeed = speed;
+      }else if(direction == 2){
+          leftSpeed = -speed;
+          rightSpeed = -speed;
+      }else if(direction == 3){
+          leftSpeed = -speed;
+          rightSpeed = speed;
+      }else if(direction == 4){
+          leftSpeed = speed;
+          rightSpeed = -speed;
+      }
+      motor_9.run((9)==M1?-(leftSpeed):(leftSpeed));
+      motor_10.run((10)==M1?-(rightSpeed):(rightSpeed));
+}
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
+MeIR ir;
+
+void setup(){
+    ir.begin();
+    move(1,50);
+    ir.sendString("hello");
+}
+
+void loop(){
+    _loop();
+}
+
+void _delay(float seconds){
+    long endTime = millis() + seconds * 1000;
+    while(millis() < endTime)_loop();
+}
+
+void _loop(){
+}
